bb15666df656655f906017d19bf54fee3d31ef5d
Harsh Parecha <harsh.parecha@strandls.com>
2023-03-07 19:59:33 +0530

    Added solution

---
+++ Problem 709 [Medium].py
@@ -0,0 +1,54 @@
+"""
+This problem was asked by Slack.
+
+You are given an N by M matrix of 0s and 1s. Starting from the top left corner, how many ways are there to reach the 
+bottom right corner?
+
+You can only move right and down. 0 represents an empty space while 1 represents a wall you cannot walk through.
+
+For example, given the following matrix:
+
+[[0, 0, 1],
+ [0, 0, 1],
+ [1, 0, 0]]
+
+Return two, as there are only two ways to get to the bottom right:
+
+    Right, down, down, right
+    Down, right, down, right
+
+The top left corner and bottom right corner will always be 0.
+"""
+
+matrix = [[0, 0, 1],
+          [0, 0, 1],
+          [1, 0, 0]]
+size = (len(matrix[0]) - 1, len(matrix) - 1)
+counter = [0]
+
+
+def solver(x: int, y: int, visited=set()):
+    if (x, y) == size:
+        counter[0] += 1
+        return
+
+    if (x, y) in visited or matrix[y][x] == 1:
+        return
+
+    visited.add((x, y))
+
+    if x < size[0]:
+        solver(x + 1, y, visited.copy())
+
+    if y < size[0]:
+        solver(x, y + 1, visited.copy())
+
+    if x > 0:
+        solver(x - 1, y, visited.copy())
+
+    if y > 0:
+        solver(x, y - 1, visited.copy())
+
+
+solver(0, 0)
+print(counter)
